7ee0e960c0942dcd870f0fce4330ca94c6d4bd43
vdtheone <vishalprajapati22052001@gmail.com>
2023-10-25 17:56:26 +0530

    move register_blueprint in create_app function

--- app.py
+++ app.py
@@ -9,10 +9,10 @@ def create_app():
     app.debug = True
     app.config['SQLALCHEMY_DATABASE_URI'] = Config.SQLALCHEMY_DATABASE_URI
     db.init_app(app)
+    app.register_blueprint(user_bp, url_prefix = "/api/users")
     return app
 
 app = create_app()
 
 if __name__ == '__main__':
-    app.register_blueprint(user_bp, url_prefix = "/api/users")
     app.run(debug=True)
