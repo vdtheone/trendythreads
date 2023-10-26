d4688a7b7904b86250381fae914241377920fe33
vdtheone <vishalprajapati22052001@gmail.com>
2023-10-26 11:54:44 +0530

    create EmailOTP tabel and remove otp column form user table

--- src/models/user.py
+++ src/models/user.py
@@ -1,7 +1,7 @@
 import uuid
 from datetime import datetime
 
-from sqlalchemy import Boolean, Column, DateTime, Integer, String
+from sqlalchemy import Boolean, Column, DateTime, ForeignKey, Integer, String
 
 from src.database import db
 
@@ -11,16 +11,26 @@ def generate_uuid():
 
 
 class User(db.Model):
-    __tablename__ = 'user'
+    __tablename__ = "user"
 
     id = Column(String, primary_key=True, index=True, default=generate_uuid())
     first_name = Column(String)
     last_name = Column(String)
-    email = Column(String, unique=True)
+    email = Column(String)
     password = Column(String)
     mobile_no = Column(String)
     is_varify = Column(Boolean, default=False)
     is_deleted = Column(Boolean, default=False)
+    created_at = Column(DateTime, default=datetime.utcnow())
+    updated_at = Column(DateTime, default=datetime.utcnow(), onupdate=datetime.utcnow())
+
+
+class EmailOTP(db.Model):
+    __tablename__ = "emailotp"
+
+    id = Column(String, primary_key=True, index=True, default=generate_uuid())
+    userid = Column(String, ForeignKey(User.id))
+    email = Column(String)
     created_at = Column(DateTime, default=datetime.utcnow())
     updated_at = Column(DateTime, default=datetime.utcnow(), onupdate=datetime.utcnow())
     otp = Column(Integer)
