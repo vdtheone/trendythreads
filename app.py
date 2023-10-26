7bf05aa5de09f7930a92948b8cb26b3662eeecb8
vdtheone <vishalprajapati22052001@gmail.com>
2023-10-26 14:38:49 +0530

    black format

--- app.py
+++ app.py
@@ -8,7 +8,7 @@ from src.routers.user import user_bp
 def create_app():
     app = Flask(__name__)
     app.debug = True
-    app.config['SQLALCHEMY_DATABASE_URI'] = Config.SQLALCHEMY_DATABASE_URI
+    app.config["SQLALCHEMY_DATABASE_URI"] = Config.SQLALCHEMY_DATABASE_URI
     db.init_app(app)
     app.register_blueprint(user_bp, url_prefix="/api/users")
     return app
@@ -16,5 +16,5 @@ def create_app():
 
 app = create_app()
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     app.run(debug=True)
--- config.py
+++ config.py
@@ -6,5 +6,5 @@ load_dotenv()
 
 
 class Config:
-    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URI')
+    SQLALCHEMY_DATABASE_URI = os.environ.get("DATABASE_URI")
     JWT_SECRET_KEY = os.environ.get("JWT_SECRET_KEY")
