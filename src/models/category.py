e882de18436774e996e7a9369c7b4644396aed89
vdtheone <vishalprajapati22052001@gmail.com>
2023-10-26 17:50:24 +0530

    crearte Product and Category table

---
+++ src/models/category.py
@@ -0,0 +1,14 @@
+from datetime import datetime
+
+from sqlalchemy import Column, DateTime, String
+
+from src.database import db
+from src.utils.generate_uuid import generate_uuid
+
+
+class Category(db.Model):
+    id = Column(String, primary_key=True, index=True, default=generate_uuid())
+    name = Column(String)
+    description = Column(String)
+    created_at = Column(DateTime, default=datetime.utcnow())
+    updated_at = Column(DateTime, default=datetime.utcnow(), onupdate=datetime.utcnow())
---
+++ src/models/product.py
@@ -0,0 +1,21 @@
+from datetime import datetime
+
+from sqlalchemy import Boolean, Column, DateTime, Float, ForeignKey, Integer, String
+
+from src.database import db
+from src.models.category import Category
+from src.utils.generate_uuid import generate_uuid
+
+
+class Product(db.Model):
+    id = Column(String, primary_key=True, index=True, default=generate_uuid())
+    category_id = Column(String, ForeignKey(Category.id))
+    name = Column(String)
+    description = Column(String)
+    price = Column(Float)
+    brand = Column(String)
+    stockquantity = Column(Integer)
+    image = Column(String)
+    active = Column(Boolean, default=False)
+    created_at = Column(DateTime, default=datetime.utcnow())
+    updated_at = Column(DateTime, default=datetime.utcnow(), onupdate=datetime.utcnow())
