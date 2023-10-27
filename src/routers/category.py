bb65fb415ee73d01bda5de4fd3b18358da46ff56
vdtheone <vishalprajapati22052001@gmail.com>
2023-10-27 16:51:41 +0530

    create route for all_categories, category_by_id, product_by_category

--- src/routers/category.py
+++ src/routers/category.py
@@ -1,6 +1,11 @@
 from flask import Blueprint
 
-from src.views.category import add_new_category
+from src.views.category import (
+    add_new_category,
+    all_categories,
+    category_by_id,
+    products_by_category,
+)
 
 category_bp = Blueprint("category", __name__)
 
@@ -8,3 +13,18 @@ category_bp = Blueprint("category", __name__)
 @category_bp.route("/addcat", methods=["POST"])
 def add_cat():
     return add_new_category()
+
+
+@category_bp.route("/", methods=["GET"])
+def get_all_categories():
+    return all_categories()
+
+
+@category_bp.route("/<cat_id>", methods=["GET"])
+def get_category_by_id(cat_id):
+    return category_by_id(cat_id)
+
+
+@category_bp.route("<cat_id>/products", methods=["GET"])
+def get_products_by_category(cat_id):
+    return products_by_category(cat_id)
