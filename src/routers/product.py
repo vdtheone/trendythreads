38a04ba8a77f04ee4ebc8b96b923bc1834a7a1dd
vdtheone <vishalprajapati22052001@gmail.com>
2023-10-27 16:52:37 +0530

    add route for produc_by_id, search product

--- src/routers/product.py
+++ src/routers/product.py
@@ -1,15 +1,25 @@
 from flask import Blueprint
 
-from src.views.product import add_new_product, all_products
+from src.views.product import add_new_product, all_products, one_product, search_product
 
 product_bp = Blueprint("product", __name__)
 
 
-@product_bp.route("/addproduct", methods=["POST"])
+@product_bp.route("/add_product", methods=["POST"])
 def add_product():
     return add_new_product()
 
 
-@product_bp.route("/", methods=["GET"])
+@product_bp.route("/all_products", methods=["GET"])
 def get_all_products():
     return all_products()
+
+
+@product_bp.route("/<product_id>", methods={"GET"})
+def product_by_id(product_id):
+    return one_product(product_id)
+
+
+@product_bp.route("/search", methods=["GET"])
+def search():
+    return search_product()
