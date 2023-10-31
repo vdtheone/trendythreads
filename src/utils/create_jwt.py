a8f64c57d3978a6f89f90e771a4e81ff067d1b49
vdtheone <vishalprajapati22052001@gmail.com>
2023-10-31 21:31:07 +0530

    create token for change user password using forgot passwoerd

--- src/utils/create_jwt.py
+++ src/utils/create_jwt.py
@@ -13,3 +13,10 @@ def create_access_token(payload):
     encoded_token = jwt.encode(payload, JWT_SECRET_KEY, ALGORITHM)
     # return f"Bearer {encoded_token}"
     return encoded_token
+
+
+def create_token_password(payload):
+    expire = datetime.utcnow() + timedelta(minutes=30)
+    payload.update({"exp": expire})
+    encoded_token = jwt.encode(payload, JWT_SECRET_KEY, ALGORITHM)
+    return encoded_token
