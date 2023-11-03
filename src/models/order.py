204a98690180ea33e06bc9f6972a40e9ebd96fda
vdtheone <vishalprajapati22052001@gmail.com>
2023-11-03 14:35:24 +0530

    create order table

---
+++ src/models/order.py
@@ -0,0 +1,20 @@
+from datetime import datetime
+
+from sqlalchemy import Column, DateTime, ForeignKey, Integer, String
+
+from src.database import db
+from src.models.product import Product
+from src.models.user import User
+from src.utils.generate_uuid import generate_uuid
+
+
+class Order(db.Model):
+    __tablename__ = "order"
+
+    id = Column(String, primary_key=True, index=True, default=generate_uuid)
+    user_id = Column(String, ForeignKey(User.id))
+    product_id = Column(String, ForeignKey(Product.id))
+    quantity = Column(Integer)
+    total_amount = Column(Integer)
+    created_at = Column(DateTime, default=datetime.utcnow())
+    updated_at = Column(DateTime, default=datetime.utcnow(), onupdate=datetime.utcnow())
