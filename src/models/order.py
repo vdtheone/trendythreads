99a00921b8595001ee7c31f9d3cda9830e9dd238
vdtheone <vishalprajapati22052001@gmail.com>
2023-11-06 09:58:02 +0530

    create OrderStatus Enum and add status column, define relationships and create serialize object

--- src/models/order.py
+++ src/models/order.py
@@ -1,11 +1,25 @@
 from datetime import datetime
+from enum import Enum
 
 from sqlalchemy import Column, DateTime, ForeignKey, Integer, String
+from sqlalchemy.orm import relationship
 
 from src.database import db
 from src.models.product import Product
 from src.models.user import User
 from src.utils.generate_uuid import generate_uuid
+
+
+class OrderStatus(str, Enum):
+    ORDER_PLACED = "Order Placed"
+    PROCESSING = "Processing"
+    SHIPPED = "Shipped"
+    DELIVERED = "Delivered"
+    CANCELLED = "Cancelled"
+    REFUNDED = "Refunded"
+    ON_HOLD = "On Hold"
+    PENDING_PAYMENT = "Pending Payment"
+    RETURNED = "Returned"
 
 
 class Order(db.Model):
@@ -16,5 +30,21 @@ class Order(db.Model):
     product_id = Column(String, ForeignKey(Product.id))
     quantity = Column(Integer)
     total_amount = Column(Integer)
+    status = Column(String)
     created_at = Column(DateTime, default=datetime.utcnow())
     updated_at = Column(DateTime, default=datetime.utcnow(), onupdate=datetime.utcnow())
+
+    # Define relationships
+    user = relationship("User", back_populates="orders")
+    product = relationship("Product", back_populates="orders")
+
+    def serialize(self):
+        return {
+            "id": self.id,
+            "user_id": self.user_id,
+            "product_id": self.product_id,
+            "quantity": self.quantity,
+            "total_amount": self.total_amount,
+            "created_at": self.created_at,
+            "updated_at": self.updated_at,
+        }
