1a07e750a9fbbbb5dd23250b4d0f0b2ec2c716e5
vdtheone <vishalprajapati22052001@gmail.com>
2023-12-07 15:51:43 +0530

    add comments for better understanding

--- app.py
+++ app.py
@@ -1,7 +1,10 @@
 from flask import Flask
+
+# Importing Flask-Limiter modules
 from flask_limiter import Limiter
 from flask_limiter.util import get_remote_address
 
+# Importing your configurations and blueprints
 from config import Config
 from src.database import db
 from src.routers.cart import cart_bp
@@ -16,27 +19,33 @@ def create_app():
 
     # Setting up Flask-Limiter
     limiter = Limiter(
-        get_remote_address,
-        app=app,
-        default_limits=["200 per day", "50 per hour"],
-        storage_uri="memory://",
+        app=app,  # Passing the Flask app instance
+        key_func=get_remote_address,  # Function to get the remote IP address
+        default_limits=["200 per day", "50 per hour"],  # Default rate limits
+        storage_uri="memory://",  # Using in-memory storage (for demonstration purposes)
     )
 
     # Configuring the user blueprint (avoiding circular import)
     configure_user_blueprint(limiter)
 
+    # Setting up app configurations
     app.debug = True
     app.config["SQLALCHEMY_DATABASE_URI"] = Config.SQLALCHEMY_DATABASE_URI
     db.init_app(app)
+
+    # Registering all blueprints with their respective URL prefixes
     app.register_blueprint(user_bp, url_prefix="/api/user")
     app.register_blueprint(product_bp, url_prefix="/api/product")
     app.register_blueprint(category_bp, url_prefix="/api/category")
     app.register_blueprint(cart_bp, url_prefix="/api/cart")
     app.register_blueprint(order_bp, url_prefix="/api/order")
+
     return app
 
 
+# Creating the app instance
 app = create_app()
 
+# Running the app only if the script is executed directly
 if __name__ == "__main__":
     app.run(debug=True)
