780deb92a1c7e03f4db993d68ebef1a613625e58
vdtheone <vishalprajapati22052001@gmail.com>
2023-12-11 17:48:45 +0530

    add new fields in serializer and return resonce according requirments

--- src/serializers/product_serializer.py
+++ src/serializers/product_serializer.py
@@ -1,16 +1,35 @@
 from flask import jsonify
 
+from src.database import db
+from src.models.category import Category
+from src.models.inventory import Inventory
+
 
 def product_serializer(product):
     return jsonify(
         {
-            "category_id": product.category_id,
+            "id": product.id,
             "name": product.name,
             "description": product.description,
             "price": product.price,
             "brand": product.brand,
-            "stockquantity": product.stockquantity,
             "image": product.image,
             "active": product.active,
+            "category": {
+                "category_id": product.category_id,
+                "category_name": db.session.query(Category)
+                .filter(Category.id == product.category_id)
+                .first()
+                .name,
+            },
+            "inventory": {
+                "stock_status": "Stock available"
+                if db.session.query(Inventory)
+                .filter(Inventory.product_id == product.id)
+                .first()
+                .stock_quantity
+                > 0
+                else "Out of stock"
+            },
         }
     )
