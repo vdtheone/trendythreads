31a1f029ed4391881c531371c6a9fb0b8b0edead
vdtheone <vishalprajapati22052001@gmail.com>
2023-12-14 10:53:47 +0530

    give proper naming convention and make query according requirements. create common serializer for single and multiple order

--- src/serializers/order_serializer.py
+++ src/serializers/order_serializer.py
@@ -1,33 +1,34 @@
 from flask import jsonify
 
 from src.database import db
-from src.models.order import Order
-from src.models.product import Product
+from src.models.order import OrderItem
 
 
-def order_serialzer(order):
-    items = db.session.query(Product).join(Order, Product.id == order.product_id).all()
-    items_serialized = []
+def order_serialzer(order_object):
+    products = (
+        db.session.query(OrderItem).filter(OrderItem.order_id == order_object.id).all()
+    )
+    products_serialized = []
     total_amount = 0
-    for item in items:
-        item_serialized = {
-            "product_id": item.id,
-            "name": item.name,
-            "quantity": order.quantity,
-            "price_per_unit": item.price,
-            "total_price": order.quantity * item.price,
+    for product in products:
+        serialized = {
+            "product_id": product.product.id,
+            "name": product.product.name,
+            "quantity": product.quantity,
+            "price_per_unit": product.product.price,
+            "total_price": product.quantity * product.product.price,
         }
-        total_amount += item_serialized["total_price"]
-        items_serialized.append(item_serialized)
+        total_amount += product.product.price * product.quantity
+        products_serialized.append(serialized)
 
     return jsonify(
         {
             "message": "Order placed successfully",
             "order_details": {
-                "order_id": order.id,
-                "user_id": order.user_id,
+                "order_id": order_object.id,
+                "user_id": order_object.user_id,
                 "status": "Order placed",
-                "items": items_serialized,
+                "products": products_serialized,
                 "total_amount": total_amount,
             },
         }
