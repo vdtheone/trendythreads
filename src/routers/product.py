dd2aed9cc984dda807de1653138349d7ad7aa341
vdtheone <vishalprajapati22052001@gmail.com>
2024-02-09 23:17:33 +0530

    add get_product_image and update_image_database route

--- src/routers/product.py
+++ src/routers/product.py
@@ -1,4 +1,4 @@
-from flask import Blueprint
+from flask import Blueprint, send_from_directory
 
 from src.views.product import (
     add_new_product,
@@ -9,9 +9,12 @@ from src.views.product import (
     product_rating_by_user,
     product_review_by_user,
     search_product,
+    update_product_image,
 )
 
 product_bp = Blueprint("product", __name__)
+
+UPLOAD_FOLDER = "E:\\Internship\\Flask\\trendythreads\\src\\static\\uploads"
 
 
 @product_bp.route("/add_product", methods=["POST"])
@@ -53,3 +56,18 @@ def review_product(product_id):
 @product_bp.route("/<product_id>/rating", methods=["POST"])
 def rating_product(product_id):
     return product_rating_by_user(product_id)
+
+
+@product_bp.route("/images/<filename>", methods=["GET"])
+def get_product_image(filename):
+    cache_control = "no-store, no-cache, must-revalidate, post-check=0, pre-check=0"
+    headers = {
+        "Cache-Control": cache_control,
+        "Expires": "0",
+    }
+    return send_from_directory(UPLOAD_FOLDER, filename), 200, headers
+
+
+@product_bp.route("/update_image/<product_id>", methods=["POST"])
+def update_image_database(product_id):
+    return update_product_image(product_id)
