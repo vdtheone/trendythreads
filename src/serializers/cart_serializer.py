a066b389ffde88bf26462d6df788aca13e04dafc
vdtheone <vishalprajapati22052001@gmail.com>
2024-02-09 23:18:11 +0530

    add product details in serializer

--- src/serializers/cart_serializer.py
+++ src/serializers/cart_serializer.py
@@ -1,11 +1,17 @@
-from flask import jsonify
+from src.database import db
+from src.models.product import Product
 
 
 def cart_serializer(cart):
-    return jsonify(
-        {
-            "user_id": cart.user_id,
-            "product_id": cart.product_id,
-            "quantity": cart.quantity,
-        }
-    ).json
+    product = db.session.query(Product).filter(Product.id == cart.product_id).first()
+
+    return {
+        "id": cart.id,
+        "user_id": cart.user_id,
+        "product_id": cart.product_id,
+        "quantity": cart.quantity,
+        "name": product.name,
+        "price": product.price,
+        "brand": product.brand,
+        "image": product.image,
+    }
