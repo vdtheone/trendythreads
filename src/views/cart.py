ad262527e7602cf9848ee0fb12edb8eff6b0252e
vdtheone <vishalprajapati22052001@gmail.com>
2024-02-09 23:21:14 +0530

    change get_all_item function and add delete_all_items and get_cart_count function

--- src/views/cart.py
+++ src/views/cart.py
@@ -1,19 +1,38 @@
-from flask import request
+from flask import g, jsonify, request
 
 from src.common_crud.crud import CRUD
+from src.database import db
 from src.models.cart import Cart
+from src.serializers.cart_serializer import cart_serializer
+from src.utils.required_jwt_token import login_required, token_required
 
 cart_crud = CRUD(Cart)
 
 
+@login_required
 def add_to_cart_item():
     data = request.json
+    data['user_id'] = g.user
     responce = cart_crud.create(data)
     return responce
 
 
+@login_required
 def get_all_item():
-    return cart_crud.list_all()
+    user_id = g.user
+    items = db.session.query(Cart).filter_by(user_id=user_id).all()
+    if not items:
+        return jsonify({"error": "No Cart found"}), 404
+
+    cart_items = [cart_serializer(item) for item in items]
+
+    return jsonify(
+        {
+            "cart_items": cart_items,
+            "total_items": len(cart_items),
+            "total_cost": sum(item["price"] * item["quantity"] for item in cart_items),
+        }
+    )
 
 
 def delete_item_from_cart(cart_item_id):
@@ -23,3 +42,17 @@ def delete_item_from_cart(cart_item_id):
 def update_cart_item(cart_item_id):
     updated_data = request.json
     return cart_crud.update(cart_item_id, updated_data)
+
+
+@token_required
+def delete_all_items(decoded_token):
+    user_id = decoded_token.get("id")
+    return cart_crud.delete_all(user_id)
+
+
+@login_required
+def get_cart_count():
+    user_id = g.user
+    items = db.session.query(Cart).filter_by(user_id=user_id).all()
+    cart_count = len(items)
+    return jsonify({'cartCount': cart_count})
