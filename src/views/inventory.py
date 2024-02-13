149a1721341df0c98bc4f09342a41eeae2d56200
vdtheone <vishalprajapati22052001@gmail.com>
2024-02-13 23:49:00 +0530

    refactor code and comments

--- src/views/inventory.py
+++ src/views/inventory.py
@@ -7,6 +7,7 @@ from src.models.inventory import Inventory
 inventory_crud = CRUD(Inventory)
 
 
+# Function to add product stock to the inventory
 def add_product_stock():
     user_input_data = request.json
     product_in_inventory = (
@@ -18,19 +19,23 @@ def add_product_stock():
         responce = inventory_crud.create(user_input_data)
         return responce
     else:
-        return jsonify(
-            {"error": "Product is already in Inventory.So update the stock if you want"}
+        error_message = (
+            "Product is already in Inventory. So update the stock if you want"
         )
+        return jsonify({"error": error_message})
 
 
+# Function to get all items from the inventory
 def get_all_item():
     return inventory_crud.list_all()
 
 
+# Function to delete an item from the inventory by ID
 def delete_item_from_inventory(inventory_item_id):
     return inventory_crud.delete(inventory_item_id)
 
 
+# Function to update an item in the inventory by ID
 def update_inventory_item(inventory_item_id):
     updated_data = request.json
     return inventory_crud.update(inventory_item_id, updated_data)
