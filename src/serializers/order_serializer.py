70929286326e777100e8525555a448c353033111
vdtheone <vishalprajapati22052001@gmail.com>
2024-02-17 00:00:22 +0530

    create new serializer for all order of customer

--- src/serializers/order_serializer.py
+++ src/serializers/order_serializer.py
@@ -1,10 +1,8 @@
-from flask import jsonify
-
 from src.database import db
 from src.models.order import OrderItem
 
 
-def order_serialzer(order_object):
+def add_new_order_serialzer(order_object):
     products = (
         db.session.query(OrderItem).filter(OrderItem.order_id == order_object.id).all()
     )
@@ -21,15 +19,57 @@ def order_serialzer(order_object):
         total_amount += product.product.price * product.quantity
         products_serialized.append(serialized)
 
-    return jsonify(
-        {
-            "message": "Order placed successfully",
-            "order_details": {
-                "order_id": order_object.id,
-                "user_id": order_object.user_id,
-                "status": "Order placed",
-                "products": products_serialized,
-                "total_amount": total_amount,
-            },
-        }
-    )
+    return {
+        "message": "Order placed successfully",
+        "order_details": {
+            "order_id": order_object.id,
+            "user_id": order_object.user_id,
+            "status": "Order placed",
+            "products": products_serialized,
+            "total_amount": total_amount,
+        },
+    }
+
+
+def all_customer_orders_serialzer(order_object):
+    products_serialized = []
+    total_amount = 0
+    serialized = {
+        "product_id": order_object.product.id,
+        "name": order_object.product.name,
+        "description": order_object.product.description,
+        "price": order_object.product.price,
+        "brand": order_object.product.brand,
+        "image": order_object.product.image,
+        "quantity": order_object.quantity,
+        "price_per_unit": order_object.product.price,
+        "total_price": order_object.quantity * order_object.product.price,
+    }
+    total_amount += order_object.product.price * order_object.quantity
+    products_serialized.append(serialized)
+
+    return {
+        "order_details": {
+            "order_id": order_object.id,
+            "total_amount": total_amount,
+            "product": serialized,
+            "total_amount": total_amount,
+            "created_at": order_object.created_at,
+            "updated_at": order_object.updated_at,
+        },
+    }
+
+
+# data = {
+#     "orders": [
+#         {
+#             "id": 1,
+#             "customer_name": "John Doe",
+#             "total_amount": 50.00,
+#             "items": [
+#                 {"id": 101, "name": "Product 1", "price": 20.00, "quantity": 2},
+#                 {"id": 102, "name": "Product 2", "price": 10.00, "quantity": 1},
+#             ],
+#         }
+#     ]
+# }
