bf83213f1e8e06843d670d0e680f4969d00903d3
vdtheone <vishalprajapati22052001@gmail.com>
2024-03-07 23:30:52 +0530

    create order_detail_serialzer

--- src/serializers/order_serializer.py
+++ src/serializers/order_serializer.py
@@ -51,6 +51,7 @@ def all_customer_orders_serialzer(order_object):
     return {
         "order_details": {
             "order_id": order_object.order_id,
+            "order_item_id": order_object.id,
             "total_amount": total_amount,
             "product": serialized,
             "total_amount": total_amount,
@@ -73,3 +74,44 @@ def all_customer_orders_serialzer(order_object):
 #         }
 #     ]
 # }
+
+
+def order_detail_serialzer(order_object):
+    total_amount = 0
+    products_serialized = {
+        "product_id": order_object.product_id,
+        "name": order_object.product.name,
+        "description": order_object.product.description,
+        "price": order_object.product.price,
+        "brand": order_object.product.brand,
+        "image": order_object.product.image,
+        "quantity": order_object.quantity,
+        "price_per_unit": order_object.product.price,
+        "total_price": order_object.quantity * order_object.product.price,
+    }
+    total_amount += order_object.product.price * order_object.quantity
+    address = {
+        "address_id": order_object.orders.address.id,
+        "first_name": order_object.orders.address.first_name,
+        "last_name": order_object.orders.address.last_name,
+        "mobile_no": order_object.orders.address.mobile_no,
+        "pincode": order_object.orders.address.pincode,
+        "address": order_object.orders.address.address,
+        "city_district_town": order_object.orders.address.city_district_town,
+        "state": order_object.orders.address.state,
+        "landmark": order_object.orders.address.landmark,
+        "alternate_mobile_no": order_object.orders.address.alernate_mobile_no,
+        "address_type": order_object.orders.address.address_type,
+    }
+
+    return {
+        "order_details": {
+            "order_id": order_object.order_id,
+            "total_amount": total_amount,
+            "product": products_serialized,
+            "address": address,
+            "total_amount": total_amount,
+            "created_at": order_object.created_at,
+            "updated_at": order_object.updated_at,
+        },
+    }
